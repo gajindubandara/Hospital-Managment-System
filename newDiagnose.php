a6b1c03da8b6c9be7e2e0adf6782bad03058354a
Ridmi211 <ridmiyatigammana@gmail.com>
2021-12-10 12:40:09 +0530

    Change page title

--- newDiagnose.php
+++ newDiagnose.php
@@ -7,7 +7,7 @@ require("logincheck.php");
 
 <head>
 
-    <title>Bootstrap Tutorial Sample Page</title>
+    <title>CS - New Diagnose</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
